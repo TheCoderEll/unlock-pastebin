a127c2855e3226faef80a252a022ed3cad6057eb
TheCoderEll <75807962+TheCoderEll@users.noreply.github.com>
2021-10-29 07:50:51 +0100

    .

--- js.js
+++ js.js
@@ -2,7 +2,10 @@ function getElementByXpath(path) {
   return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
-while (true){ 
+function hi(){ 
   document.getElementById("postpasswordverificationform-password").value = Math.random().toString(36).substring(2, Math.random()) + Math.random().toString(36).substring(2, Math.random());
   getElementByXpath("/html/body/div[1]/div[2]/div[1]/div[2]/div[2]/form/div[3]/button").click();
+  setTimeout(() => {  hi(); }, 100);
 }
+
+hi();
